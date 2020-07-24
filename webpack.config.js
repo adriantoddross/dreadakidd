289b4c76a0479c32482e7f9c64ee9235f53439b9
Adrian Ross <adriantoddross@Adrians-MacBook-Pro.local>
2020-07-24 10:53:03 -0400

    Set up html-loader; move images to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
