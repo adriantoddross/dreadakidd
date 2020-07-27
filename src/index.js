958781ae32869d81171d7c17cd335c46c845a4af
Adrian Ross <adriantoddross@gmail.com>
2020-07-27 11:56:49 -0400

    Add html-webpack-plugin to generate html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
+import html from "./index.html";
 import "./styles/index.css";
 import "./styles/screen.css";
-import html from "../dist/index.html";
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     app: "./src/index.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Drea Da Kidd, American rapper and songwriter",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
