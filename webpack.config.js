37aa75f96f77a875acea8e0d7df7d5e486bba2d2
Adrian Ross <adriantoddross@gmail.com>
2020-07-27 12:49:21 -0400

    Clean webpack build when building dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,7 @@ module.exports = {
     app: "./src/index.js",
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Drea Da Kidd, American rapper and songwriter",
       filename: "index.html",
